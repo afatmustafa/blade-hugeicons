df7220cf316fab5c7d44ddb196a0c872e861475b
Mustafa Afat <mustafa@afat.me>
2024-12-21 03:26:36 +0300

    feat: add 222 new icons from hugeicons package
    - Added 222 new icons
    - Added tests for new icons
    - Icons sourced from hugeicons package latest version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,11 +1,34 @@
 # Changelog
 
-This changelog follows [the Keep a Changelog standard](https://keepachangelog.com).
+All notable changes to `blade-hugeicons` will be documented in this file
 
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased](https://github.com/blade-organization/blade-heroicons/compare/1.0.0...main)
 
+## v0.0.4 - 2024-05-21
 
-## 1.0.0 (20??-??-??)
+### Added New 222 Icons
+- Added new icons: [`activity-01`](https://hugeicons.com/icon/activity-01-stroke-rounded), [`activity-02`](https://hugeicons.com/icon/activity-02-stroke-rounded), [`activity-03`](https://hugeicons.com/icon/activity-03-stroke-rounded), [`activity-04`](https://hugeicons.com/icon/activity-04-stroke-rounded), [`ai-beautify`](https://hugeicons.com/icon/ai-beautify-stroke-rounded), [`ai-browser`](https://hugeicons.com/icon/ai-browser-stroke-rounded), [`ai-cloud`](https://hugeicons.com/icon/ai-cloud-stroke-rounded), [`ai-eraser`](https://hugeicons.com/icon/ai-eraser-stroke-rounded), [`ai-game`](https://hugeicons.com/icon/ai-game-stroke-rounded), [`ai-generative`](https://hugeicons.com/icon/ai-generative-stroke-rounded), [`ai-idea`](https://hugeicons.com/icon/ai-idea-stroke-rounded), [`ai-image`](https://hugeicons.com/icon/ai-image-stroke-rounded), [`ai-magic`](https://hugeicons.com/icon/ai-magic-stroke-rounded), [`ai-mail-01`](https://hugeicons.com/icon/ai-mail-01-stroke-rounded), [`ai-mail-02`](https://hugeicons.com/icon/ai-mail-02-stroke-rounded), [`ai-programming`](https://hugeicons.com/icon/ai-programming-stroke-rounded), [`ai-setting`](https://hugeicons.com/icon/ai-setting-stroke-rounded), [`ai-user`](https://hugeicons.com/icon/ai-user-stroke-rounded), [`ai-video`](https://hugeicons.com/icon/ai-video-stroke-rounded), [`airplane-seat`](https://hugeicons.com/icon/airplane-seat-stroke-rounded), [`airport`](https://hugeicons.com/icon/airport-stroke-rounded), [`amazon`](https://hugeicons.com/icon/amazon-stroke-rounded), [`apple-vision-pro`](https://hugeicons.com/icon/apple-vision-pro-stroke-rounded), [`arrange-by-letters-az`](https://hugeicons.com/icon/arrange-by-letters-az-stroke-rounded), [`arrange-by-letters-za`](https://hugeicons.com/icon/arrange-by-letters-za-stroke-rounded), [`assignments`](https://hugeicons.com/icon/assignments-stroke-rounded), [`beach-02`](https://hugeicons.com/icon/beach-02-stroke-rounded), [`biometric-access`](https://hugeicons.com/icon/biometric-access-stroke-rounded), [`biometric-device`](https://hugeicons.com/icon/biometric-device-stroke-rounded), [`blocked`](https://hugeicons.com/icon/blocked-stroke-rounded), [`brandfetch`](https://hugeicons.com/icon/brandfetch-stroke-rounded), [`cafe`](https://hugeicons.com/icon/cafe-stroke-rounded), [`call-02`](https://hugeicons.com/icon/call-02-stroke-rounded), and more.
 
-Initial release.
+### Tests
+- Added tests for all new icons
+
+## v0.0.3 - 2023-07-05
+
+### Changed
+* Added compatibility for Laravel 10 by adding support for PHP 8.1 by @swapnilsarwe in #1
+
+### New Contributors
+* @swapnilsarwe made their first contribution in #1
+
+## v0.0.2 - 2023-05-04
+
+### Added
+- Add missing menu-restaurant, trade-down and trade-up icons.
+
+## v0.0.1 - 2023-04-14
+
+### Added
+- Initial release.
---
+++ tests/NewIconsTest.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests;
+
+use Afatmustafa\HugeIcons\BladeHugeIconsServiceProvider;
+use BladeUI\Icons\BladeIconsServiceProvider;
+use Orchestra\Testbench\TestCase;
+
+class NewIconsTest extends TestCase
+{
+    /** @test */
+    public function it_compiles_a_single_anonymous_component()
+    {
+        $result = svg('hugeicons-activity-01')->toHtml();
+
+        // Note: the empty class here seems to be a Blade components bug.
+        $expected = <<<'SVG'
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" color="currentColor"><path d="M4.31802 19.682C3 18.364 3 16.2426 3 12C3 7.75736 3 5.63604 4.31802 4.31802C5.63604 3 7.75736 3 12 3C16.2426 3 18.364 3 19.682 4.31802C21 5.63604 21 7.75736 21 12C21 16.2426 21 18.364 19.682 19.682C18.364 21 16.2426 21 12 21C7.75736 21 5.63604 21 4.31802 19.682Z" stroke="currentColor"></path><path d="M7 14L9.79289 11.2071C10.1834 10.8166 10.8166 10.8166 11.2071 11.2071L12.7929 12.7929C13.1834 13.1834 13.8166 13.1834 14.2071 12.7929L17 10" stroke="currentColor"></path></svg>
+            SVG;
+
+        $this->assertSame($expected, $result);
+    }
+
+    /** @test */
+    public function it_can_add_classes_to_icons()
+    {
+        $result = svg('hugeicons-activity-01', 'w-6 h-6 text-gray-500')->toHtml();
+
+        $expected = <<<'SVG'
+            <svg class="w-6 h-6 text-gray-500" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" color="currentColor"><path d="M4.31802 19.682C3 18.364 3 16.2426 3 12C3 7.75736 3 5.63604 4.31802 4.31802C5.63604 3 7.75736 3 12 3C16.2426 3 18.364 3 19.682 4.31802C21 5.63604 21 7.75736 21 12C21 16.2426 21 18.364 19.682 19.682C18.364 21 16.2426 21 12 21C7.75736 21 5.63604 21 4.31802 19.682Z" stroke="currentColor"></path><path d="M7 14L9.79289 11.2071C10.1834 10.8166 10.8166 10.8166 11.2071 11.2071L12.7929 12.7929C13.1834 13.1834 13.8166 13.1834 14.2071 12.7929L17 10" stroke="currentColor"></path></svg>
+            SVG;
+
+        $this->assertSame($expected, $result);
+    }
+
+    /** @test */
+    public function it_can_add_styles_to_icons()
+    {
+        $result = svg('hugeicons-activity-01', ['style' => 'color: #555'])->toHtml();
+
+        $expected = <<<'SVG'
+            <svg style="color: #555" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" color="currentColor"><path d="M4.31802 19.682C3 18.364 3 16.2426 3 12C3 7.75736 3 5.63604 4.31802 4.31802C5.63604 3 7.75736 3 12 3C16.2426 3 18.364 3 19.682 4.31802C21 5.63604 21 7.75736 21 12C21 16.2426 21 18.364 19.682 19.682C18.364 21 16.2426 21 12 21C7.75736 21 5.63604 21 4.31802 19.682Z" stroke="currentColor"></path><path d="M7 14L9.79289 11.2071C10.1834 10.8166 10.8166 10.8166 11.2071 11.2071L12.7929 12.7929C13.1834 13.1834 13.8166 13.1834 14.2071 12.7929L17 10" stroke="currentColor"></path></svg>
+            SVG;
+
+        $this->assertSame($expected, $result);
+    }
+
+    protected function getPackageProviders($app)
+    {
+        return [
+            BladeIconsServiceProvider::class,
+            BladeHugeIconsServiceProvider::class,
+        ];
+    }
+}
